a647e1e1194a503da00503465d1162746ac293b6
karlines <karlines@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-03-12 14:21:49 +0000

    cleaned up code and help files!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diagram@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/shape/R/getellipse.R
@@ -0,0 +1,17 @@
+
+##==============================================================================
+## getellipse   : calculates the x-y values for (part of) an ellipse
+##==============================================================================
+
+getellipse <- function (rx=1, ry=rx, mid=c(0,0), dr=0.01,
+  angle=0, from=-pi, to=pi) {
+
+  x  <- c( seq(from,to,by=dr), to)
+  if (x[length(x)] == x[length(x)-1])
+    x <- x[-length(x)]
+  xy <- cbind( mid[1] + rx * cos(x), mid[2] + ry * sin(x))
+
+  if (angle != 0)
+    xy <- rotatexy (xy, angle=angle, mid=mid)  # rotate around mid
+  return(xy)
+}
